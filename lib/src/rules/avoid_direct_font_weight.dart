808e820ff2eeb9d388ee7ad9b3e571b6e629f8d7
mogaming <seiya.orz@gmail.com>
2025-07-31 10:38:50 +0900

    fix: change severity to error

--- lib/src/rules/avoid_direct_font_weight.dart
+++ lib/src/rules/avoid_direct_font_weight.dart
@@ -1,14 +1,16 @@
 import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/error/error.dart' as e;
 import 'package:analyzer/error/listener.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 
 class AvoidDirectFontWeight extends DartLintRule {
   AvoidDirectFontWeight() : super(code: _code);
 
-  static const _code = LintCode(
+  static final _code = LintCode(
     name: 'avoid_direct_font_weight',
     problemMessage: 'Avoid using FontWeight directly. Use CustomFontWeight instead.',
     correctionMessage: 'Replace FontWeight with CustomFontWeight',
+    errorSeverity: e.ErrorSeverity.ERROR,
   );
 
   @override
@@ -20,8 +22,7 @@ class AvoidDirectFontWeight extends DartLintRule {
     context.registry.addSimpleIdentifier((node) {
       if (node.name == 'FontWeight' && node.parent is! PrefixedIdentifier) {
         final element = node.staticElement;
-        if (element != null &&
-            element.library?.source.uri.toString() == 'dart:ui') {
+        if (element != null && element.library?.source.uri.toString() == 'dart:ui') {
           reporter.atNode(node, code);
         }
       }
@@ -30,8 +31,7 @@ class AvoidDirectFontWeight extends DartLintRule {
     context.registry.addPrefixedIdentifier((node) {
       if (node.prefix.name == 'FontWeight') {
         final element = node.prefix.staticElement;
-        if (element != null &&
-            element.library?.source.uri.toString() == 'dart:ui') {
+        if (element != null && element.library?.source.uri.toString() == 'dart:ui') {
           reporter.atNode(node.prefix, code);
         }
       }
